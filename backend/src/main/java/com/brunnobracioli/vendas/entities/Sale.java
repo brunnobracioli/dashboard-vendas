f669a3f15089a8c13deb90336f70af4112654a6f
brunnobracioli <brunno_012@hotmail.com>
2021-05-07 00:15:19 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/brunnobracioli/vendas/entities/Sale.java
@@ -0,0 +1,47 @@
+package com.brunnobracioli.vendas.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_Sales")
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller; // composição das entidades (chave estrangeira
+	
+	public Sale() {
+	}
+
+	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+
+	
+	
+	
+	
+	
+
+}
