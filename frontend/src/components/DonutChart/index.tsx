bd5f3288f6b2b33104a88eaaa3b04b22c4fb6103
brunnobracioli <brunno_012@hotmail.com>
2021-05-09 14:47:29 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -9,39 +10,54 @@ type ChartData = {
 }
 
 function DonutChart() {
-    
-    //FORMA ERRADA
-    let chartData : ChartData = {labels: [], series: []};
+
+    // FORMA CORRETA PARA TRABALHAR COM ESTADOS E CONTROLE DE CHAMADA DA API
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+
+            });
+    }, []);
 
     //FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    //let chartData : ChartData = {labels: [], series: []};
 
-            chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData);
-        });
+    //FORMA ERRADA
+    //axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    //  .then(response => {
+    //      const data = response.data as SaleSum[];
+    //      const myLabels = data.map(x => x.sellerName);
+    //     const mySeries = data.map(x => x.sum);
+
+    //      setChartData({labels: myLabels, series: mySeries});
+    //      console.log(chartData);
+    //  });
 
     //const mockData = {
-      //  series: [477138, 499928, 444867, 220426, 473088],
-        //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //  series: [477138, 499928, 444867, 220426, 473088],
+    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
         }
     }
 
-        return (
-            <Chart
-                options={{...options, labels: chartData.labels}} // ... informa para pegar tudo do obj options reaproveita ele, só que você pode colocar mais coisas nesse objeto. 
-                series={chartData.series}
-                type="donut"
-                height="240"
-            />
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }} // ... informa para pegar tudo do obj options reaproveita ele, só que você pode colocar mais coisas nesse objeto. 
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
     );
 }
 
