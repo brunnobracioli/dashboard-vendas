55d5bb2d5d13de3dce2e093801152a26682b7eca
brunnobracioli <brunno_012@hotmail.com>
2021-05-09 21:34:37 -0300

    DataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -64,17 +64,17 @@ function BarChart() {
             },
         };
         
-        const mockData = { // mockData são usados quando utilizamos dados estáticos.
-            labels: {
-                categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-            },
+        /* const mockData = { // mockData são usados quando utilizamos dados estáticos.
+           labels: {
+               categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+          },
             series: [
                 {
                     name: "% Sucesso",
                     data: [43.6, 67.1, 67.7, 45.6, 71.1]
                 }
             ]
-        };
+        }; */
 
         return (
             <Chart
